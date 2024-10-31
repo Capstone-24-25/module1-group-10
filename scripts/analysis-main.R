d72075bbab1d35e537d8f0ab82d25362a907ade4
OwenPhilliber1 <owenphilliber@ucsb.edu>
2024-10-30 20:23:03 -0700

    Wrote up question 4 and added code for question 4 to analysis-main.R

--- scripts/analysis-main.R
+++ scripts/analysis-main.R
@@ -165,61 +165,37 @@ ranked_proteins <- tibble(
   pull(protein)
 
 ##------- Question 4 -----------------------------------------------------------
-# get names
-var_names <- read_csv('biomarker-raw.csv', 
-                      col_names = F, 
-                      n_max = 2, 
-                      col_select = -(1:2),
-                      show_col_types = FALSE) %>%
-  t() %>%
-  as_tibble() %>%
-  rename(name = V1, 
-         abbreviation = V2) %>%
-  na.omit()
+biomarker_sstar <- biomarker_clean %>%
+  select(group, any_of(ranked_proteins)) %>%
+  mutate(class = (group == 'ASD')) %>%
+  select(-group)
 
-# function for trimming outliers (good idea??)
-trim <- function(x, .at){
-  x[abs(x) > .at] <- sign(x[abs(x) > .at])*.at
-  return(x)
-}
+# Define the full model with all predictors
+initial_model <- glm(class ~ 1, data = biomarker_sstar, family = binomial)
+full_model <- glm(class ~ ., data = biomarker_sstar, family = binomial)
 
-# read in data
-biomarker_clean <- read_csv('biomarker-raw.csv', 
-                            skip = 2,
-                            col_select = -2L,
-                            col_names = c('group', 
-                                          'empty',
-                                          pull(var_names, abbreviation),
-                                          'ados'),
-                            show_col_types = FALSE,
-                            na = c('-', '')) %>%
-  filter(!is.na(group)) %>%
-  # log transform, center and scale, and trim
-  mutate(across(.cols = -c(group, ados), 
-                ~ scale(log10(.x))[, 1])) %>%
-  # reorder columns
-  select(group, ados, everything())
+forward_model <- stats::step(initial_model, direction='forward', scope = list(lower = initial_model, upper = full_model))
 
+backward_model <- glm(class ~ ., data = biomarker_sstar, family = binomial) %>% stats::step(direction = "backward")
 
-#exploratory analysis of outlying values
-outliers <- biomarker_clean %>%
-  rowwise() %>%
-  mutate(across(.cols = c(-group,ados),
-                ~ abs(.x) > 3)) %>%
-  mutate(num_outlier = sum(c_across(-c(group, ados)))) %>%
-  select(group, num_outlier)
+#Stepwise regression
+both_model <- glm(class ~ ., data = biomarker_sstar, family = binomial)%>% stats::step(direction = "both", scope = list(lower = initial_model, upper = full_model))
 
-summary <- outliers %>%
-  group_by(group) %>%
-  summarize(outlier_mean = mean(num_outlier),
-            outlier_sd = sd(num_outlier),
-            outlier_max = max(num_outlier),
-            outlier_total = sum(num_outlier),
-            outlier_subjects = sum(num_outlier > 0))
+summary(forward_model)
+summary(backward_model)
+summary(both_model)
 
-outliers
-summary
+par(mfrow = c(2, 2))
+plot(forward_model$residuals, main = "Forward Residuals", ylab = "Residuals")
+plot(backward_model$residuals, main = "Backward Residuals", ylab = "Residuals")
+plot(both_model$residuals, main = "Both-Direction Residuals", ylab = "Residuals")
 
-# export as r binary
-save(list = 'biomarker_clean', 
-     file = 'data/biomarker-clean.RData')
+# Comparing to the list of 20 proteins:
+
+full_model <- glm(class ~ ., data = biomarker_sstar, family = binomial)
+reduced_model <- glm(class ~ DERM + IgD + PTN + RELT + CSK + `14-3-3 protein theta`, data = biomarker_sstar, family = binomial)
+
+summary(full_model)
+summary(reduced_model)
+
+anova(full_model, reduced_model)
